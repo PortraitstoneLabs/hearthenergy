ec5c1e3e6fe1a8d5c3a75005f8d7fefc47dc8c47
khristophekeen <khristophe.keen@gmail.com>
2024-10-18 12:38:04 -0700

    EmailJS updated

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: 'Hearth Energy',
           from_email: form.email,
-          to_email: 'contact@hearthenergy.org',
+          to_email: 'energyhearth@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
