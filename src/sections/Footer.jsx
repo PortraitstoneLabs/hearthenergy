ef0e1173502df2e99382392dced5494514a0b311
khristophekeen <khristophe.keen@gmail.com>
2024-10-19 10:57:31 -0700

    Updated footer padding

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => (
   <section
     className={`${styles.flexCenter} ${styles.paddingY} flex-col`}
     style={{ //backdropFilter: "blur(35px)",
-    paddingLeft: "50px", 
-    paddingRight: "50px",}} >
+    paddingLeft: "40px", 
+    paddingRight: "40px",}} >
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#56555b]">
       <p className="font-Roboto font-normal text-center text-[18px] leading-[27px] text-black">
         Copyright Ⓒ 2024 Portraitstone LLC. All Rights Reserved.
