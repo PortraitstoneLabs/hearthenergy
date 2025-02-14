a758ec227d04a6c2759ae0030d08b253655a7bb3
khristophekeen <khristophe.keen@gmail.com>
2025-02-13 18:24:40 -0800

    Updated hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,9 +30,9 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-black text-center font-Roboto">
-          Welcome to Hearth Energy <span className="spinning-hand">♻️</span>
+          Imagine Without Thinking<span className="spinning-hand">♻️</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Auditing the Solar Industry</p>
+        <p className="hero_tag text-gray_gradient">Light Is Imagination</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
